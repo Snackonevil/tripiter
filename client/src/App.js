ce1ba6015dede4b2d7133a6a28dee3147f1d911f
Kevin Lacson <lacsonky@gmail.com>
2022-03-31 20:53:56 -0400

    start react-router-dom routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 function App() {
     return (
-        <div>
-            <h1>TRIPITER</h1>
-        </div>
+        <Router>
+            <div>
+                <h1>TRIPITER</h1>
+            </div>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </Router>
     );
 }
 
