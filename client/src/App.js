ae9f0eed6bcbf1122d7f6dae0ff63d621eae7f40
Christine Nguyen <cnhnguyen59@gmail.com>
2022-04-02 14:40:28 -0400

    created tripboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./contexts/AuthContext";
-import LoginPage from "./pages/LoginPage";
+/* import AuthProvider from "./contexts/AuthContext";
+import LoginPage from "./pages/LoginPage"; */
+import TripBoard from "./pages/TripBoard";
 import "./App.css";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
                     <h1>TRIPITER</h1>
                 </div>
                 <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<LoginPage />} />
+                    {/* <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<LoginPage />} /> */}
+                    <Route path="/tripboard" element={<TripBoard />} />
                 </Routes>
             </Router>
         </AuthProvider>
---
+++ client/src/pages/TripBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+
+const tripBoard = () => {
+  return (
+    <div>tripPage</div>
+  )
+}
+
+export default tripBoard
