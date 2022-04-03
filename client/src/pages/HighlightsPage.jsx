8f614297b7ff7d25ed0764e694e921d8f2d10193
Alyssa Cuson <acuson98@gmail.com>
2022-04-03 17:16:21 -0400

    adding divs for highlight image, title, description, and publishing info

--- client/src/pages/HighlightsPage.jsx
+++ client/src/pages/HighlightsPage.jsx
@@ -4,7 +4,18 @@ import { Link } from "react-router-dom";
 
 const highlights = () => {
     return (
-        <div></div>
+        <div>
+            <div class="highlight-img">
+                <img src="#" alt="Highlight Image" />
+            </div>
+            <div class="description-box">
+                <h1 class="highlight-title">Highlight Title</h1>
+                    <h2 class="publish-date">Published on this date.</h2>
+                    {/* For this part, consider putting their name/username and profile picture */}
+                    <h2 class="user-name">Username</h2>
+                        <p class="highlight-description">Insert your description here.</p>
+            </div>
+        </div>
     )
 };
 
