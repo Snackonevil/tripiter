7fd9866b699f4f217ff48149c16703ebbd76b6ef
Alyssa Cuson <acuson98@gmail.com>
2022-04-03 14:57:01 -0400

    added react imports and starting html content

---
+++ client/src/pages/HighlightsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+
+const highlights = () => {
+    return (
+        <div></div>
+    )
+};
+
+export default highlights;
