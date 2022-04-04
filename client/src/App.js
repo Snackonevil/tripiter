bbee2514e3ac5c11864f544819829e665a94e69a
Christine Nguyen <cnhnguyen59@gmail.com>
2022-04-03 20:15:24 -0400

    created trip board

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./contexts/AuthContext";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
+/* import AuthProvider from "./contexts/AuthContext"; */
+/* import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage"; */
+import TripBoard from "./pages/TripBoard";
 import "./App.css";
 
 function App() {
     return (
-        <AuthProvider>
+        <>
+        {/* <AuthProvider> */}
             <Router>
                 <div>
                     <h1>TRIPITER</h1>
                 </div>
                 <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignUpPage />} />
+                   {/*  <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUpPage />} /> */}
+                    <Route path="/tripboard" element={<TripBoard />} />
                 </Routes>
             </Router>
-        </AuthProvider>
+        {/* </AuthProvider> */}
+        </>
     );
 }
 
