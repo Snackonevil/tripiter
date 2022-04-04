dc16293d728ce7e392d2bc34873649dc2a7fd781
Kevin Lacson <lacsonky@gmail.com>
2022-04-03 20:44:28 -0400

    navbar/tripboard branches

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-/* import AuthProvider from "./contexts/AuthContext"; */
-/* import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage"; */
-=======
 import AuthProvider from "./contexts/AuthContext";
 import LoginPage from "./pages/LoginPage";
->>>>>>> 1f9496f8d1260bdd0f6aafe13d238c4bca6453cf
 import TripBoard from "./pages/TripBoard";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -14,21 +8,16 @@ import Navbar from "./components/Navbar";
 function App() {
     return (
         <>
-        {/* <AuthProvider> */}
-            <Router>
-                <Navbar />
-                <Routes>
-<<<<<<< HEAD
-                   {/*  <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignUpPage />} /> */}
-=======
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<LoginPage />} />
->>>>>>> 1f9496f8d1260bdd0f6aafe13d238c4bca6453cf
-                    <Route path="/tripboard" element={<TripBoard />} />
-                </Routes>
-            </Router>
-        {/* </AuthProvider> */}
+            <AuthProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<LoginPage />} />
+                        <Route path="/tripboard" element={<TripBoard />} />
+                    </Routes>
+                </Router>
+            </AuthProvider>
         </>
     );
 }
