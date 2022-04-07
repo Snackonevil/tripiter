bc3baafed98732f55987f515416f84ceac56d570
Kevin Lacson <lacsonky@gmail.com>
2022-04-06 23:25:41 -0400

    refactor privateroute for react-router 6

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate, Route, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-export default function PrivateRoute({ component: Component, ...rest }) {
-  const { currentUser } = useAuth();
-
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        currentUser ? <Component {...props} /> : <Redirect to="/login" />;
-      }}
-    ></Route>
-  );
+export default function PrivateRoute({ currentUser, children }) {
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
 }
