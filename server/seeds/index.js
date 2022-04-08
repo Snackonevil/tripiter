91c8b62e6ac13fb5a6b5ca0d4df949087b9cff48
Kevin Lacson <lacsonky@gmail.com>
2022-04-07 21:31:36 -0400

    seed test user, 3 trips, and 3 hightlights to one trip

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -5,27 +5,50 @@ const userData = require('./userData.json');
 const tripData = require('./tripData.json');
 const highlightData = require('./highlightData.json');
 
-async function seedUsers() {
-  await User.insertMany(userData);
-  console.log('Users seeded!');
+async function seedTestUser() {
+  await User.create({
+    username: 'Test User',
+    password: 'iamatestuser',
+    first_name: 'Test',
+    last_name: 'User',
+    email: 'Test@test.com',
+    trips: [],
+  });
+  console.log('Test user seeded!');
 }
 
 async function seedTrips() {
-  await Trip.insertMany(tripData);
-
+  for (const trip of tripData) {
+    const newTrip = await Trip.create(trip);
+    await User.findOneAndUpdate(
+      { username: 'Test User' },
+      { $push: { trips: newTrip._id } }
+    );
+  }
   console.log('Trips seeded!');
 }
+
 async function seedHighlights() {
-  const seedHighlights = await Highlight.insertMany(highlightData);
+  for (const highlight of highlightData) {
+    const newHighlight = await Highlight.create(highlight);
+    await Trip.findOneAndUpdate(
+      { name: 'FUN FUN FRANCE' },
+      { $push: { hightlights: newHighlight._id } }
+    );
+  }
   console.log('Highlights seeded!');
 }
+
 db.on('error', err => console.log(err));
+
 db.once('open', async () => {
   await User.deleteMany({});
   await Trip.deleteMany({});
   await Highlight.deleteMany({});
 
-  await seedUsers();
+  await seedTestUser();
+  await seedTrips();
+  await seedHighlights();
 
   process.exit(0);
 });
