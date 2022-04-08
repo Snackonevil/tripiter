b2082c97bebec0aadfc3328cd767f2405e6df81d
Kevin Lacson <lacsonky@gmail.com>
2022-04-07 20:26:53 -0400

    working on seeds

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,32 +1,32 @@
 const db = require('../config/connection');
-const { Users, Trips, Highlights } = require('../models');
+const { User, Trip, Highlight } = require('../models');
 
 const userData = require('./userData.json');
 const tripData = require('./tripData.json');
 const highlightData = require('./highlightData.json');
 
 db.once('open', async () => {
-  await Users.deleteMany({});
+  await User.deleteMany({});
 
-  const seedUsers = await Users.insertMany(userData);
+  const seedUsers = await User.insertMany(userData);
 
   console.log('Users seeded!');
   process.exit(0);
 });
 
 db.once('open', async () => {
-  await Trips.deleteMany({});
+  await Trip.deleteMany({});
 
-  const seedTrips = await Trips.insertMany(tripData);
+  const seedTrips = await Trip.insertMany(tripData);
 
   console.log('Trips seeded!');
   process.exit(0);
 });
 
 db.once('open', async () => {
-  await Highlights.deleteMany({});
+  await Highlight.deleteMany({});
 
-  const seedHighlights = await Highlights.insertMany(highlightData);
+  const seedHighlights = await Highlight.insertMany(highlightData);
 
   console.log('Highlights seeded!');
   process.exit(0);
