e550e2607de905757aa8fb26aa9354dfc7ae4d88
Kevin Lacson <lacsonky@gmail.com>
2022-04-10 14:53:44 -0400

    encrypt password to db

--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,53 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
     username: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     first_name: {
-      type: String,
-      require: true,
+        type: String,
+        require: true,
     },
     last_name: {
-      type: String,
-      require: false,
+        type: String,
+        require: false,
     },
     email: {
-      type: String,
-      require: true,
-      unique: true,
+        type: String,
+        require: true,
+        unique: true,
     },
     picture: {
-      type: String,
+        type: String,
     },
     trips: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Trip',
-      },
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Trip',
+        },
     ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
-);
+})
 
-const User =  model('User', userSchema);
-module.exports = User;
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+
+    next()
+})
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+const User = model('User', userSchema)
+
+module.exports = User
