bb6de295e4d57fe3da32e143894bc79d609864b6
Kevin Lacson <lacsonky@gmail.com>
2022-04-09 22:32:13 -0400

    create or update profile bones

--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react';
-import ProgressBar from './ProgressBar';
+import { useState } from 'react'
+import ProgressBar from './ProgressBar'
 
 export default function UploadImage() {
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
+    const [file, setFile] = useState(null)
+    const [upload, setUpload] = useState(false)
+    const [error, setError] = useState(null)
 
-  const types = ['image/png', 'image/jpeg'];
+    // File type validation
+    const types = ['image/png', 'image/jpeg']
 
-  function handleChange(e) {
-    let selected = e.target.files[0];
+    function handleChange(e) {
+        let selected = e.target.files[0]
 
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError('');
-    } else {
-      setFile(null);
-      setError('Please select an image file (png or jpg)');
+        if (selected && types.includes(selected.type)) {
+            setFile(selected)
+            setError('')
+        } else {
+            setFile(null)
+            setError('Please select an image file (png or jpg)')
+        }
     }
-  }
 
-  return (
-    <form>
-      <label>
-        <input type="file" onChange={handleChange} />
-        <span>+</span>
-      </label>
-      <div className="output">
-        {error && <div className="error">{error}</div>}
-        {file && <div>{file.name}</div>}
-        {file && <ProgressBar file={file} setFile={setFile} />}
-      </div>
-    </form>
-  );
+    // Fire upload by rendering progressbar
+    function handleUpload(e) {
+        e.preventDefault()
+        setUpload(true)
+    }
+
+    return (
+        <form className="upload-form">
+            <label>
+                <input type="file" onChange={handleChange} />
+                <span>+</span>
+            </label>
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+                {file && <button onClick={handleUpload}>Upload</button>}
+                {upload && (
+                    <ProgressBar
+                        file={file}
+                        setFile={setFile}
+                        setUpload={setUpload}
+                    />
+                )}
+            </div>
+        </form>
+    )
 }
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,53 +1,53 @@
-import { useState, useEffect, useContext, createContext } from 'react';
-import firebaseApp from '../config/firebase';
+import { useState, useEffect, createContext } from 'react'
+import firebaseApp from '../config/firebase'
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-} from 'firebase/auth';
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+} from 'firebase/auth'
 
-export const AuthContext = createContext();
-const provider = new GoogleAuthProvider();
-const auth = getAuth(firebaseApp);
+export const AuthContext = createContext()
+const provider = new GoogleAuthProvider()
+const auth = getAuth(firebaseApp)
 
 export default function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState(null)
 
-  async function login(email, password) {
-    return await signInWithEmailAndPassword(auth, email, password);
-  }
+    async function login(email, password) {
+        return await signInWithEmailAndPassword(auth, email, password)
+    }
 
-  async function signUp(name, email, password) {
-    return await createUserWithEmailAndPassword(auth, email, password);
-  }
+    async function signUp(name, email, password) {
+        return await createUserWithEmailAndPassword(auth, email, password)
+    }
 
-  async function googleAuth() {
-    return await signInWithPopup(auth, provider);
-  }
+    async function googleAuth() {
+        return await signInWithPopup(auth, provider)
+    }
 
-  function signOutUser() {
-    signOut(auth);
-    setCurrentUser(null);
-  }
+    function signOutUser() {
+        signOut(auth)
+        setCurrentUser(null)
+    }
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user);
-    });
-    return unsubscribe;
-  }, []);
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setCurrentUser(user)
+        })
+        return unsubscribe
+    }, [])
 
-  const value = {
-    currentUser,
-    setCurrentUser,
-    login,
-    signUp,
-    googleAuth,
-    signOutUser,
-  };
+    const value = {
+        currentUser,
+        setCurrentUser,
+        login,
+        signUp,
+        googleAuth,
+        signOutUser,
+    }
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
