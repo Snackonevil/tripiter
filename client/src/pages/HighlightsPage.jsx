9cd2bd6f8241459675cbbc4862f972f481384295
Alyssa Cuson <acuson98@gmail.com>
2022-04-10 18:32:19 -0400

    h2 to h4

--- client/src/pages/HighlightsPage.jsx
+++ client/src/pages/HighlightsPage.jsx
@@ -11,9 +11,9 @@ const highlights = () => {
             <div className="description-box">
                 <h1 className="highlight-title">Highlight Title</h1>
                     <h2 className='location'>Location</h2>
-                        <h4 className="publish-date">Published on this date.</h2>
+                        <h4 className="publish-date">Published on this date.</h4>
                     {/* For this part, consider putting their name/username and profile picture */}
-                        <h4 className="user-name">Username</h2>
+                        <h4 className="user-name">Username</h4>
                             <p className="highlight-description">Insert your description here.</p>
             </div>
         </div>
