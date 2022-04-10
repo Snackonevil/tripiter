73796be9ad69e401bc4ccb3e4a148ee279823b94
Kevin Lacson <lacsonky@gmail.com>
2022-04-10 17:58:53 -0400

    remove import

--- client/src/pages/TripBoard.jsx
+++ client/src/pages/TripBoard.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import AddHighlight from '../components/AddHighlight'
 import Highlight from '../components/Highlight'
-import avatar from "../images/user-placeholder.png"
-import highlights from "../utils/highlightsData"
+import avatar from '../images/user-placeholder.png'
 
-
-export default function TripBoard () {
-  return (
-    <div className="parent">
-      <div className="user-info">
-            <img src={avatar} alt="avatar" />
-            <h1>TRIP NAME</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
-                debitis aliquam, nesciunt quidem error asperiores labore veniam?
-                Consectetur dicta provident, aliquam porro inventore quidem
-                voluptatem doloribus quisquam optio natus quibusdam?
-            </p>
+export default function TripBoard() {
+    return (
+        <div className="parent">
+            <div className="user-info">
+                <img src={avatar} alt="avatar" />
+                <h1>TRIP NAME</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Nam debitis aliquam, nesciunt quidem error asperiores labore
+                    veniam? Consectetur dicta provident, aliquam porro inventore
+                    quidem voluptatem doloribus quisquam optio natus quibusdam?
+                </p>
+            </div>
+            <div className="filter">
+                <h1>## Highlights</h1>
+            </div>
+            <div className="trip-board">
+                {highlights.map((highlight) => {
+                    return (
+                        <Highlight key={highlight.id} highlight={highlight} />
+                    )
+                })}
+            </div>
+            <AddHighlight />
         </div>
-        <div className="filter">
-            <h1>## Highlights</h1>
-        </div>
-        <div className="trip-board">
-            {highlights.map((highlight)=>{
-              return <Highlight key={highlight.id} highlight={highlight} />
-            })}
-        </div>
-        <AddHighlight />
-    </div>
-  )
+    )
 }
