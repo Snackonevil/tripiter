752ab50024493fe7629064fabdc688897f7d553e
Kevin Lacson <lacsonky@gmail.com>
2022-04-13 15:17:02 -0400

    ready to test private component

--- client/src/components/PrivateComponent.jsx
+++ client/src/components/PrivateComponent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Auth from '../utils/auth'
 
 export default function PrivateComponent({ children, userId }) {
-    const { _id } = Auth.getProfile()
-    if (userId === _id) {
+    const { data } = Auth.getProfile()
+    if (userId === data._id) {
         return children
     } else {
         return ''
