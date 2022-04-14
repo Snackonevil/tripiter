94c05c7a5e731f71d2d9cbd680afc9a799ffec20
Darby Johnson <darbyjjohnson1@gmail.com>
2022-04-13 20:49:43 -0400

    set up trip and create trip for mutation update/delete-- and drug in sass

--- client/src/components/Trip.jsx
+++ client/src/components/Trip.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { HiOutlineTrash } from 'react-icons/hi'
+import { FiSettings } from 'react-icons/fi'
+import { useNavigate } from 'react-router';
+import TripModal from './TripModal';
 
 export default function Trip({ trip }) {
     const navigate = useNavigate()
@@ -8,17 +11,26 @@ export default function Trip({ trip }) {
         e.preventDefault()
         navigate(`/trip/${trip._id}`)
     }
+    function deleteTrip(){
+        var result = window.confirm("Are you sure you want to delete?");
+        if (result){
+          console.log('deleted')
+        }
+      }
+    
+      function updateTrip(){
+        console.log('updated')
+      }
 
     return (
         <>
             <div className="trip" onClick={handleClick}>
-                <img src={trip.img_url} alt="trip-thumbnail" />
-                <h4 style={{ fontSize: '1.75rem', textAlign: 'center' }}>
-                    {trip.name}
-                </h4>
-                <p style={{ color: 'lightgray', textAlign: 'center' }}>
-                    {trip.highlights.length} highlights
-                </p>
+                <img src={`${trip.img_url}`} alt="trip-thumbnail" />
+                <div className='tripName'><p>{trip.name}</p></div>
+                <div className="edit-btns">
+                  <button className="buttons" onClick={updateTrip}><FiSettings /></button>
+                  <button className="buttons" onClick={deleteTrip}><HiOutlineTrash /></button>
+                </div>
             </div>
         </>
     )
